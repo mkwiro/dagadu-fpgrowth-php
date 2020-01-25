bc282405abc1d10191fb18ac89f764c8a0920673
M K Wiro <muhammad.wiro@students.amikom.ac.id>
2020-01-25 07:02:45 +0700

    sidebar

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -6,5 +6,40 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?= $data['judul'];?></title>
     <link rel="stylesheet" href="<?=BASEURL;?>/css/bootstrap.css">
+    <style>
+.sidenav {
+  height: 100%;
+  width: 189px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: #222a2d;
+}
+.sidenav a {
+  text-align: center;
+  border-bottom: 1px solid;
+  font-size: 13px;
+  color: #b1bac1;
+  display: block;
+  line-height: 36px;
+}
+.sidenav a:hover {
+  color: #222a2d;
+  background-color: #85c0ec;
+}
+.main {
+  margin-left: 200px; /* Same as the width of the sidenav */
+}
+@media screen and (max-height: 450px) {
+  .sidenav {padding-top: 15px;}
+  .sidenav a {font-size: 18px;}
+}
+</style>
 </head>
 <body>
+<div class="sidenav">
+  <a style="background-color: red"><?= $data['judul'];?></a>
+  <a href="#">Transaksi</a>
+  <a href="#">Barang</a>
+  <a href="#">Analisis</a>
+</div>
