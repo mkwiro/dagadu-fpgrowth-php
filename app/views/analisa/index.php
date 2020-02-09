533962bcac9ed65ffd2ebf28694c679d7bb82f64
M K Wiro <muhammad.wiro@students.amikom.ac.id>
2020-02-09 12:20:46 +0700

    tampilan itemset versi 4

--- app/views/analisa/index.php
+++ app/views/analisa/index.php
@@ -602,6 +602,48 @@ if(isset($_POST['submit']) and $error==''){
 ?>
 
 <!-- TAMPILAN -->
+
+<div class="mt-5" style=" margin-left: 200px">
+<div class="row">
+        <div class="col-xl-12">
+            <div class="card shadow mb-4">
+            <!-- Card Header - Dropdown -->
+            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+              <h6 class="m-0 font-weight-bold text-primary">Itemset</h6>
+            </div>
+            <!-- Card Body -->
+            <div class="card-body">
+            <div style="overflow-x:auto;width:100%;">
+            <table class="table table-striped table-hover table-bordered" style="table-layout: fixed; width: 100%;">
+                <thead>
+                    <tr>
+                        <th class="text-center" width="100">TRANSAKSI</th>
+                        <?php 
+                        for($i=0;$i<count($barang);$i++){
+                            echo '<th class="text-center" width="50">'.$kode_barang[$barang[$i]].'</th>';
+                        }
+                        ?>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php echo $daftar_tabulasi;?>
+                    <tr>
+                        <td>Frekuensi</td>
+                        <?php 
+                        for($i=0;$i<count($barang);$i++){
+                            echo '<th class="text-center">'.$frekuensi[$barang[$i]].'</th>';
+                        }
+                        ?>
+                    </tr>
+                </tbody>
+            </table>
+            </div>
+            </div>
+            </div>
+        </div>
+        </div>
+        </div>
+
 <div class="mt-5" style=" margin-left: 200px">
         <div class="col-xl-12 col-lg-7">
             <div class="card shadow mb-4">
